37905487e3e406e23b10bbaef890a160b3ff1dc0
marcelo-04 <mclg.nogueira@gmail.com>
2022-06-27 19:16:01 -0300

    Criando a 1ª Servlet, front-end e back-end e model de login

---
+++ src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import model.ModelLogin;
+
+// O chamado Controller são as servlets ou ServletLoginController
+@WebServlet("/ServletLogin") // Mapeamento de URL que vem da tela
+public class ServletLogin extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    public ServletLogin() {
+        super();
+    }
+    
+    // Recebe os dados pela URL em parâmetros
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+	}
+	
+	// Recebe os dados enviados por um formulário
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+		
+		} else {
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			request.setAttribute("msg", "Por gentileza digite o login e senha corretamente!");
+			redirecionar.forward(request, response);
+		}
+	}
+
+}
