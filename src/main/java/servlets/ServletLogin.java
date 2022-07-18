db3c15b2152b11abe60a33ee55bedecc27d418ce
marcelo-04 <mclg.nogueira@gmail.com>
2022-07-18 18:55:52 -0300

    Criando o Logout e encerramento do usuário no sistema

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -29,7 +29,15 @@ public class ServletLogin extends HttpServlet {
 	// Recebe os dados pela URL em parâmetros
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		String acao = request.getParameter("acao");
+		
+		if(acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate();// invalida a a sessão
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+		} else {
+			doPost(request, response);
+		}	
 	}
 
 	// Recebe os dados enviados por um formulário
